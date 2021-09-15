9e84a6948b7f9909b17ed38f8d1c9fb6556dec69
Shamim Saif <shamimmsas7@gmail.com>
2021-09-15 22:01:28 +0600

    Changed the __str__() method because of typo:

--- Ecommerce_Project/App_Payment/models.py
+++ Ecommerce_Project/App_Payment/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.conf import settings
-# Create your models here.
 
+# Create your models here.
 class BillingAddress(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     address = models.CharField(max_length=264, blank=True)
